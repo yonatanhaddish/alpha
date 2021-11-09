208a6ba9a9c870fb61c463f32bc7785496e86d47
Yonatan Haddish <yonihailshn@gmail.com>
2021-11-09 14:03:42 -0500

    structure nav section

---
+++ src/components/Navbar/Navbar.elements.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+// export const Nav = styled.nav `
+//     background: #101522;
+//     color: red;
+//     height: 80px;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     font-size: 1.2rem;
+// `;
+
+export const Nav = styled.nav `
+    background-color: grey;
+`
+
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Nav } from './Navbar.elements';
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                
+            </Nav>  
+        </>
+    );
+};
+
+export default Navbar;
