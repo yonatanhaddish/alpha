d2168d94f71e25afbd1e372f83ab7d85dc10238f
Yonatan Haddish <yonihailshn@gmail.com>
2021-11-11 14:26:39 -0500

    nav menu on nav section created and styled

--- src/components/Navbar/Navbar.elements.js
+++ src/components/Navbar/Navbar.elements.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import GlobalStyle from '../../globalStyles';
+// import GlobalStyle from '../../globalStyles';
 import { Container } from '../../globalStyles';
 import { FaAdn } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
@@ -51,6 +51,64 @@ export const MobileIcon = styled.div`
         transform: translate(-100%, 60%);
         font-size: 1.8rem;
         cursor: pointer;
+    }
+`;
+
+export const NavMenu = styled.ul`
+    display: flex;
+    align-items: center;
+    list-style: none;
+    text-align: center;
+
+    @media screen and (max-width: 960px) {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 90vh;
+        position: absolute;
+        top: 80px;
+        left: ${({click}) => (click ? '0' : '-100%')};
+        opacity: 1;
+        transition: all 0.5s ease;
+        background: #101522;
+    }
+`;
+
+export const NavItem = styled.li`
+    height: 80px;
+    border-bottom: 2px solid transparent;
+
+    &:hover {
+        border-bottom: 2px solid #4b59f7;
+    }
+
+    @media screen and (max-width: 960px) {
+        width: 100%;
+
+        &:hover {
+            border: none;
+        }
+    }
+`;
+
+export const NavLinks = styled(Link)`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    padding: 0.5rem 1rem;
+    height: 100%;
+
+    @media screen and (max-width: 960px) {
+        text-align: center;
+        padding: 2rem;
+        width: 100%;
+        display: table;
+
+        &:hover {
+            color: #4b59f7;
+            transition: all 0.3s ease;
+        }
     }
 `;
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon } from './Navbar.elements';
+import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon, NavMenu, NavItem, NavLinks } from './Navbar.elements';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
@@ -19,6 +19,23 @@ const Navbar = () => {
                     <MobileIcon onClick= {handleClick}>
                         {click ? <FaTimes /> : <FaBars />}
                     </MobileIcon>
+                    <NavMenu onClick={handleClick} click={click}>
+                        <NavItem>
+                            <NavLinks to="/">
+                                Home
+                            </NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="/services">
+                                Services
+                            </NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="/products">
+                                Products
+                            </NavLinks>
+                        </NavItem>
+                    </NavMenu>
                 </NavbarContainer>
             </Nav>  
         </>
