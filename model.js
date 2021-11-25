0d376e3789f9fbc4db2081cfe0f437e0996006f0
Yonatan Haddish <yonihailshn@gmail.com>
2021-11-25 16:58:07 -0500

    some adjustments

--- model.js
+++ model.js
@@ -1,3 +1,18 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import { Container, Button } from '../../globalStyles';
+// import {
+//   InfoSec,
+//   InfoRow,
+//   InfoColumn,
+//   TextWrapper,
+//   TopLine,
+//   Heading,
+//   Subtitle,
+//   ImgWrapper,
+//   Img
+// } from './InfoSection.elements';
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Button } from '../../globalStyles';
