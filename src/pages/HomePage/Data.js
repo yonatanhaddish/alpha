5ba451bb492953c307c2b03ff90953087684cfa3
Yonatan Haddish <yonihailshn@gmail.com>
2021-11-30 09:39:59 -0500

    adding infoSection into Home page section

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,4 +1,12 @@
 export const homeObjOne= {
     lightBg: false,
-    imgStart: false
+    imgStart: '',
+    primary: true,
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'We help business oweners increase their revenue. Our team of unique specialist can help you achieve your business goals',
+    headline: 'Lead Generation Specialist for Online Businesses',
+    lightText: true,
+    topLine: 'Marketing Agency'
 };
