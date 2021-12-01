1d7af752dbae9543ccdb84532cc6b9cb019ff7fe
Yonatan Haddish <yonihailshn@gmail.com>
2021-11-30 20:52:07 -0500

    added rowItems

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,11 +1,14 @@
 import react from "react";
 import {InfoSection} from "../../components";
-import { homeObjOne } from "./Data";
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "./Data";
 
 const Home = () => {
     return (
         <>
-           <InfoSection {...homeObjOne}/> 
+           <InfoSection {...homeObjOne}/>
+           <InfoSection {...homeObjTwo}/>
+           <InfoSection {...homeObjThree}/>
+           <InfoSection {...homeObjFour}/>
         </>
     )
 }
