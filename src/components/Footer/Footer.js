6054dde426fd8be8b7033f28281aed56d39e040e
Yonatan Haddish <yonihailshn@gmail.com>
2021-12-07 11:05:35 -0500

    first section of Footer created and styled

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import { FooterContainer, FooterSubscription, FooterSubHeading, FooterSubText} from './Footer.elements'
+import React from "react";
+import { Button } from "../../globalStyles";
+import {
+  FooterContainer,
+  FooterSubscription,
+  FooterSubHeading,
+  FooterSubText,
+  Form,
+  FormInput,
+} from "./Footer.elements";
 
 const Footer = () => {
-    return (
-        <FooterContainer>
-            <FooterSubscription>
-                <FooterSubHeading>Join our exclusive membership to recieve the latest news and trends</FooterSubHeading>
-                <FooterSubText>You can unsubscribe at any time.</FooterSubText>
-                {/* <Form>
-                    <FormInput name="email" type="email" placeholder="Your Email"></FormInput>
-                    <Button fontBig>Subscribe</Button>
-                </Form> */}
-            </FooterSubscription>
-        </FooterContainer>
-    )
-}
+  return (
+    <FooterContainer>
+      <FooterSubscription>
+        <FooterSubHeading>
+          Join our exclusive membership to recieve the latest news and trends
+        </FooterSubHeading>
+        <FooterSubText>You can unsubscribe at any time.</FooterSubText>
+        <Form>
+          <FormInput
+            name="email"
+            type="email"
+            placeholder="Your Email"
+          ></FormInput>
+          <Button fontBig>Subscribe</Button>
+        </Form>
+      </FooterSubscription>
+    </FooterContainer>
+  );
+};
 
-export default Footer
+export default Footer;
